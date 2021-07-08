d07dca9def562a2b3cbbdb447188f3b1dc45d8e1
giorgiOdishvili <odishvili.giorgi@gmail.com>
2021-07-08 09:20:48 +0400

    bottom_navigation

--- app/src/main/java/com/android/shopapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/shopapp/ui/home/HomeFragment.kt
@@ -6,27 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.football.base.BaseFragment
 import com.android.shopapp.R
+import com.android.shopapp.databinding.HomeFragmentBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeFragmentBinding>(HomeFragmentBinding::inflate) {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
 
-    private lateinit var viewModel: HomeViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun start(inflater: LayoutInflater, container: ViewGroup?) {
+//        binding.bottomNavigation.OnNavigationItemSelectedListener { item ->
+//            when(item.itemId) {
+//                R.id. -> {
+//                    // Respond to navigation item 1 click
+//                    true
+//                }
+//                R.id.item2 -> {
+//                    // Respond to navigation item 2 click
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 
 }
